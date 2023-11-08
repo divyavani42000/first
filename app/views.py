6278b1bf9864be436fb7aa062f3853a5927d7832
divyavani42000 <divyavani42000@gmail.com>
2023-11-08 10:38:37 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def jampandu(request):
+    return HttpResponse('Hii jammm')
+
+def jigelrani(request):
+    return HttpResponse('how r u')
